87879ec547f87014c936a2d9ae3c92902d7dc073
ZiXyos <killian.moudery@gmail.com>
2022-07-12 22:04:05 +0200

    add: user service base + mongoose docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, User],
 })
 export class AppModule {}
