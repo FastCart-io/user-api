b2e4e9882d1848855c2e9cd7f4138c3db6fd8c52
ZiXyos <killian.moudery@gmail.com>
2022-11-07 16:54:24 +0900

    hotfix(CORS): setting up the cors configuation

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,13 @@ import { getLevel } from './utils/logger.class';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: getLevel(process.env.NODE_ENV),
-    cors: true,
+    cors: {
+      origin: '*',
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    }
   });
 
   app.enableCors();
